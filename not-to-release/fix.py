437d88bc497db6b276c12922e8b56fe4c8c4f4bc
aryamanarora <aryaman.arora2020@gmail.com>
2022-01-10 00:12:01 -0500

    remove obl subtypes, fix :pass subtypes

--- not-to-release/fix.py
+++ not-to-release/fix.py
@@ -2,7 +2,17 @@ from depedit import DepEdit
  
 infile = open("../pa_puntb-ud-test.conllu")
 d = DepEdit()
-d.add_transformation("func=/compound:lvc/;func=/nmod/;func=/.*/\t#1>#2;#3>#1\t#3>#2;#2:func=obj")
+
+# RULES
+# any that have been commented out have been already run and are unnecessary now
+
+## fix genitive objects of compound:lvc constructions
+# d.add_transformation("func=/compound:lvc/;func=/nmod/;func=/.*/\t#1>#2;#3>#1\t#3>#2;#2:func=obj")
+
+d.add_transformation("func=/aux:pass/;func=/nsubj:pass/;func=/.*/\t#3>#1;#3>#2\t#2:func=obl:agent")
+d.add_transformation("func=/nsubj:pass/\tnone\t#1:func=obl:agent")
+d.add_transformation("func=/aux:pass/;func=/obj/;func=/.*/\t#3>#1;#3>#2\t#2:func=nsubj:pass")
+
 result = d.run_depedit(infile)
 with open('pa_puntb-ud-test.conllu', 'w') as fout:
     fout.write(result)
